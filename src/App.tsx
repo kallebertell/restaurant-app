56de54dfef7b746b47012ab6cb3cda65320a154c
kalle bertell <carlpetter.bertell@gmail.com>
2018-07-08 11:26:40 +0200

    add styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,39 @@
 import * as React from 'react';
 import { Route, Switch } from 'react-router';
-import './App.css';
-
-import logo from './logo.svg';
+import styled from 'styled-components';
 
 const PageHome = () => <div>Welcome to pizza</div>
 const PageRestaurantListing = () => <div>Here we have restaurants</div>
 const PageRestaurantDetails = () => <div>Here's a specific restaurant</div>
 const PageNotFound = () => <div>Page not Found</div>;
 
+const AppWrapper = styled.div`
+  text-align: center;
+`
+const Header = styled.header`
+  background: black;
+  color: white;
+  padding: 1rem;
+`
+const Title = styled.h1`
+  text-transform: uppercase;
+  margin: 0;
+`;
+
 class App extends React.Component {
   public render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
+      <AppWrapper>
+        <Header>
+          <Title>Eat Pizza</Title>
+        </Header>
         <Switch>
           <Route exact={true} path="/" component={PageHome}/>
           <Route exact={true} path="/restaurants/:city/:postCode" component={PageRestaurantListing}/>
           <Route exact={true} path="/restaurant/:id" component={PageRestaurantDetails}/>
           <Route path="*" component={PageNotFound} />
         </Switch>
-      </div>
+      </AppWrapper>
     );
   }
 }
