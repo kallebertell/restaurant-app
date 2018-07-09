5571d52d991bd5f538231555bac052ecaf86a778
kalle bertell <carlpetter.bertell@gmail.com>
2018-07-09 10:50:19 +0200

    removed superfluous styles

--- src/App.tsx
+++ src/App.tsx
@@ -9,19 +9,6 @@ import PageRestaurantListing from 'pages/restaurantListing/PageRestaurantListing
 import * as routePaths from 'pages/routePaths';
 import * as styles from 'styles';
 
-const AppWrapper = styled.div`
-  text-align: center;
-`
-const Header = styled.header`
-  background: ${styles.COLOR_BG_DARK};
-  color: ${styles.COLOR_BRAND_SECONDARY};
-  padding: ${styles.PAD};
-`
-const Title = styled.h1`
-  text-transform: uppercase;
-  margin: 0;
-`;
-
 const Container = styled.div`
   margin: ${styles.PAD_VERY_BIG};
 `;
@@ -29,19 +16,14 @@ const Container = styled.div`
 export default class App extends React.Component {
   public render() {
     return (
-      <AppWrapper>
-        <Header>
-          <Title>Eat Pizza</Title>
-        </Header>
-        <Container>
-          <Switch>
-            <Route exact={true} path={routePaths.HOME_PATH} component={PageHome}/>
-            <Route exact={true} path={routePaths.RESTAURANT_LISTING_PATH} component={PageRestaurantListing}/>
-            <Route exact={true} path={routePaths.RESTAURANT_DETAILS_PATH} component={PageRestaurantDetails}/>
-            <Route path="*" component={PageNotFound} />
-          </Switch>
-        </Container>
-      </AppWrapper>
+      <Container>
+        <Switch>
+          <Route exact={true} path={routePaths.HOME_PATH} component={PageHome}/>
+          <Route exact={true} path={routePaths.RESTAURANT_LISTING_PATH} component={PageRestaurantListing}/>
+          <Route exact={true} path={routePaths.RESTAURANT_DETAILS_PATH} component={PageRestaurantDetails}/>
+          <Route path="*" component={PageNotFound} />
+        </Switch>
+      </Container>
     );
   }
 }
